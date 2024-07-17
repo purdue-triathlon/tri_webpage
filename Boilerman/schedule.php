3feaa692fb60009bc7728c0b4e208800abd4d7a7
Evan <47926489+Evanz142@users.noreply.github.com>
2024-07-17 09:28:18 -0400

    Update schedule.php

--- Boilerman/schedule.php
+++ Boilerman/schedule.php
@@ -32,72 +32,65 @@
             <p>**All times listed are in the US Eastern time zone, which is the same time zone as West Lafayette and Monticello.**</p>
             
               <h2>Saturday, September 28th, 2024</h2>
-              <!-- <h3>Packet Pickup</h3>
+
+              <h3>4:00 - 5:45pm or 8:00 - 9:00pm</h3>
+              <p>Packet Pickup</p>
               <p>Pick up your packet on Saturday to speed up your check-in on Sunday morning!</p>
-              <p>Complimentary bike inspections are availiable at packet pickup. Please see more information in the participant's guide (coming soon).</p?
-              <br>
-              <p>4:00 - 5:30pm or 7:30 - 9:00pm</p>
-              <p>*Packet Pickup times are subject to change! Please check this website (refresh the page) before packet pickup to confirm the times.*
+              <p>Make sure to bring your student ID (collegiate) or ID (non-collegiate) and your USAT card.</p>
+              <p>*Packet Pickup times are subject to change! Please check this website (refresh the page) before packet pickup to confirm the times.*</p>
               <br>
               <p><i>Location: Hodson’s Bay Company Bike Shop</i></p>
-              <p>360 Brown St., West Lafayette, IN 47906</p> -->
+              <p>360 Brown St., West Lafayette, IN 47906</p>
               
               
               <br><br>
               
               <h2>Sunday, September 29th, 2024</h2>
               
-              <!-- <h3>6:45 AM</h3>
+              <h3>6:45 AM</h3>
               <p>Parking Lot Opens</p>
+              <br>
+              <p><i>Race Location: Whtie Oaks RV and Campground</i></p>
+              <p>12084 N White Oaks Dr, Monticello, IN 47960</p>
               <br>
               
               <h3>7:00 AM</h3>
-              <p>Regular Check-In Opens</p>
-              <p>Express Check-In Opens (for athletes who attended Packet Pickup)</p>
-              <p>Day-of Registration Opens (cash only)</p>
-              <p>Transition Opens (for checked in members)</p>
-              <p><i>Location: Race Hub</i></p>
+              <p>Regular Check-In Opens (for athletes who did not attend packet pickup)</p>
+              <p>Express Check-In Opens (for athletes who attended packet pickup)</p>
+              <p>Day-of Registration Opens (cash only! must also have ID and USAT card)</p>
+              <p>Transition Opens (for checked in athletes)</p>
               <br>
-              
-              <h3>8:00 AM</h3>
-              <p>Day-of Registration Closes</p>
-              <br>
-              
-              <h3>8:10 AM</h3>
-              <p>Regular Check-In Closes</p>
-              <br>              
+                   
  
               <h3>8:25 AM</h3>
-              <p>Express Check-In Closes</p>
+              <p>Registration / Check-in Closes</p>
+              <br>
+
+              <h3>8:40 AM</h3>
+              <p>Transition Closes</p>
               <br>
               
-              <h3>8:40 AM</h3>
-              <p>Pre-Race Meeting</p>
-              <p><i>Location: TBA</i></p>
+              <h3>8:45 AM</h3>
+              <p>Pre-Race meeting on the beach</p>
+                <p>*REQUIRED FOR EVERYONE*</p>
               
               <br>
               
               <h3>9:00 AM</h3>
-              <p> Wave 1 Race Start (Collegiate Men, Age-group Men, Relays)</p>
-              <p><i>Location: Race Hub (Beach)</i></p>
-              
+              <p> Wave 1 Race Start (Collegiate Men, Age-group Men, Relays)</p>              
               <br>
               
               <h3>9:03 AM</h3>
-              <p> Wave 2 Race Start (Collegiate Women, Age-group women)</p>
-              <p><i>Location: Race Hub (Beach)</i></p>              
-              
+              <p> Wave 2 Race Start (Collegiate Women, Age-group women)</p>              
               <br>
               
-              <h3>1:03 PM</h3>
+              <!--<h3>1:03 PM</h3>
               <p>Time cutoff to finish the race</p>
               
-              <br>
+              <br> -->
               
-              <h3>1:20 PM</h3>
+              <h3>12:00 PM</h3>
               <p>Awards Ceremony</p>
-              <p><i>Location: Race Hub (Boat Ramp)</i></p> -->
-
 
             </div>
           </div>
